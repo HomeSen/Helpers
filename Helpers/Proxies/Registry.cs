92b7684cc501c41bc56f5a0655d4293fc130f961
Patrick Walker <mail@homesen.de>
2011-03-17 21:28:34 +0100

    * defined Registry interfaces * added empty proxy classes for DuckTyping

---
+++ Helpers/Proxies/Registry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomeSen.Helpers.Interfaces.Registry;
+
+namespace HomeSen.Helpers.Proxies
+{
+    class Registry : IRegistry
+    {
+    }
+}
